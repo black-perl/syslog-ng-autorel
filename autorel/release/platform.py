70fe59dd12a5603d49c592dbaaf074800a278756
Ankush Sharma <ankprashar@gmail.com>
2016-08-18 13:44:54 -0400

    Fixed branch names and tags name issues

--- autorel/release/platform.py
+++ autorel/release/platform.py
@@ -43,11 +43,12 @@ class GithubPlatform(object):
         """
             Get the SHA value of the commit tagged with tag
         """
-        if not tag.startwith("tags/"):
+        repo = self._client.get_repo('balabit/syslog-ng') ##hardcoded
+        if not tag.startswith("tags/"):
             tag = "tags/" + tag
-        ref_object = self._repo.get_git_ref(tag)
+        ref_object = repo.get_git_ref(tag)
         tag_sha = ref_object.object.sha
-        tag_object = self._repo.get_git_tag(tag_sha)
+        tag_object = repo.get_git_tag(tag_sha)
         linked_commit_sha = tag_object.object.sha
         return linked_commit_sha
 
@@ -55,8 +56,9 @@ class GithubPlatform(object):
         """
             Get the latest release tag
         """
+        repo = self._client.get_repo('balabit/syslog-ng') ##hardcoded
         releases = []
-        for release in self._repo.get_releases():
+        for release in repo.get_releases():
             releases.append(release)
         current_release = releases[0]
         return current_release.tag_name
@@ -65,9 +67,9 @@ class GithubPlatform(object):
         """
             Creates a new branch from the orig_branch
         """
-        if not orig_branch.startwith("heads/"):
+        if not orig_branch.startswith("heads/"):
             orig_branch = "heads/" + orig_branch
-        if not new_branch.startwith("refs/heads/"):
+        if not new_branch.startswith("refs/heads/"):
             new_branch = "refs/heads/" + new_branch
         # Get the SHA value of the latest commit on the orig_branch
         orig_branch_ref = self._repo.get_git_ref(orig_branch)
@@ -76,12 +78,14 @@ class GithubPlatform(object):
                                   sha=latest_commit_sha
                                   )
 
-    def create_commit(self, orig_branch, file_path, commit_message):
+    def create_commit(self, orig_branch, file_path, commit_message, file_name):
         """
             Creates a commit to the orig_branch by adding a file given by file_path
             and using a provided commit_message
         """
         ## find the latest commit SHA on the orig_branch
+        if not orig_branch.startswith("heads/"):
+            orig_branch = "heads/" + orig_branch
         orig_branch_ref = self._repo.get_git_ref(orig_branch)
         latest_commit_sha = orig_branch_ref.object.sha
         ## find the base tree to be written out
@@ -91,7 +95,7 @@ class GithubPlatform(object):
         with open(file_path,'r') as f:
             file_contents = f.read()
         # specifications required to create a new tree
-        new_tree_specs = InputGitTreeElement(path=file_path,
+        new_tree_specs = InputGitTreeElement(path=file_name,
                                              mode="100644",
                                              type="blob",
                                              content=file_contents
@@ -104,8 +108,8 @@ class GithubPlatform(object):
         created_commit = self._repo.create_git_commit(message=commit_message,
                                                       tree=new_tree,
                                                       parents=parent_commits,
-                                                      author=self._committer,
-                                                      committer=self._committer
+                                                      author=self._contributor,
+                                                      committer=self._contributor
                                                       )
         ## update the orig_branch to reference created_commit
         orig_branch_ref.edit(sha=created_commit.sha)
@@ -115,25 +119,25 @@ class GithubPlatform(object):
         """
             Sends a pull request from branch_a to branch_b
         """
-        self._repo.create_pull_request(title=title,
-                                       body=body,
-                                       head=branch_a,
-                                       base=branch_b
-                                       )
+        self._repo.create_pull(title=title,
+                               body=body,
+                               head=branch_a,
+                               base=branch_b
+                               )
 
-    def create_annoted_tag(self, tag, message, ref_sha, ref_type):
+    def create_annoted_tag(self, tag_name, message, ref_sha, ref_type):
         """
             Creates an annoted tag
         """
         # create a tag object
-        tag = self._repo.create_git_tag(tag=tag,
+        tag = self._repo.create_git_tag(tag=tag_name,
                                         message=message,
                                         object=ref_sha,
                                         type=ref_type,
-                                        tagger=self._committer
+                                        tagger=self._contributor
                                         )
         # Register the tag object
-        tag_ref = "refs/tags/{0}".format(tag)
+        tag_ref = "refs/tags/{0}".format(tag_name)
         self._repo.create_git_ref(ref=tag_ref,
                                   sha=tag.sha
                                   )
