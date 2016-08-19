4de14ba2cd47ac2f87c21f685e020c6aa410b9c1
Ankush Sharma <ankprashar@gmail.com>
2016-08-19 16:12:29 -0400

    global autorel settings

---
+++ autorel/settings.py
@@ -0,0 +1,15 @@
+## GLOBAL SETTINGS
+import os
+
+
+PROJECT = "black-perl/syslog-ng"
+
+GITHUB_AUTH_TOKEN = ""
+
+VERSION = "7.00"
+
+RELEASE_NAME = "new_release"
+
+RELEASE_TAG = "new_release"
+
+AUTOREL_PATH = os.path.abspath(os.path.dirname("__file__"))
