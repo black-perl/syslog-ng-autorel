940b8a44327b4952ec26913e0bf7e75b334bcf29
Ankush Sharma <ankcodes@gmail.com>
2018-12-06 20:54:33 +0530

    Made package safe for concurrent use and code refactoring in constructors

--- internal/pkg/cache/main.go
+++ internal/pkg/cache/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
 	"sync"
 	"time"
 
@@ -17,6 +16,7 @@ import (
 type Cache struct {
 	storage     *cache.Cache
 	journalFile string
+	mutex       sync.RWMutex
 }
 
 var cacheInstance *Cache
@@ -25,16 +25,10 @@ var once sync.Once
 var (
 	defaultItemExpirationTime = 24 * 60 * 7 * time.Minute
 	defaultCleanUpInterval    = 10 * time.Minute
-
-	// ItemExpirationTimeInMins provide the item expiration time
-	ItemExpirationTimeInMins = "ITEM_EXPIRATION_TIME_IN_MINS"
-
-	// CleanUpIntervalTimeInMins provide the item cleanup time after expiration
-	CleanUpIntervalTimeInMins = "CLEAN_UP_INTERVAL_TIME_IN_MINS"
 )
 
-// New returns a configured Cache instance
-func New(filename string, config map[string]string) (Cache, error) {
+// New returns a Cache instance
+func New(filename string) (*Cache, error) {
 	var errorInInstantiation error
 	once.Do(func() {
 		// load the disk persisted cache
@@ -44,62 +38,60 @@ func New(filename string, config map[string]string) (Cache, error) {
 		}
 		var persistedCacheItems map[string]cache.Item
 		json.Unmarshal(bytesRead, persistedCacheItems)
+		cacheInstance = &Cache{
+			storage:     cache.NewFrom(defaultItemExpirationTime, defaultCleanUpInterval, persistedCacheItems),
+			journalFile: filename,
+		}
+	})
+	return cacheInstance, errorInInstantiation
+}
 
-		// check if config is passed or not
-		var itemExpirationTime, cleanUpInterval time.Duration
-		val, ok := config[ItemExpirationTimeInMins]
-		if ok {
-			valStr, err := strconv.Atoi(val)
-			if err != nil {
-				errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to parse %s to integer provided as %s", val, ItemExpirationTimeInMins))
-			} else {
-				itemExpirationTime = time.Duration(valStr) * time.Minute
-			}
-		} else {
-			itemExpirationTime = defaultItemExpirationTime
+// NewWithDefaults returns a configured Cache instance
+func NewWithDefaults(filename string, itemExpirationTime time.Duration, cleanUpInterval time.Duration) (*Cache, error) {
+	var errorInInstantiation error
+	once.Do(func() {
+		// load the disk persisted cache
+		bytesRead, err := ioutil.ReadFile(filename)
+		if err != nil {
+			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to load cache from the file %s", filename))
 		}
-		val, ok = config[CleanUpIntervalTimeInMins]
-		if ok {
-			valStr, err := strconv.Atoi(val)
-			if err != nil {
-				errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to parse %s to integer provided as %s", val, CleanUpIntervalTimeInMins))
-			} else {
-				cleanUpInterval = time.Duration(valStr) * time.Minute
-			}
-		} else {
-			cleanUpInterval = defaultCleanUpInterval
-		}
+		var persistedCacheItems map[string]cache.Item
+		json.Unmarshal(bytesRead, persistedCacheItems)
 		cacheInstance = &Cache{
 			storage:     cache.NewFrom(itemExpirationTime, cleanUpInterval, persistedCacheItems),
 			journalFile: filename,
 		}
 	})
-	return *cacheInstance, errorInInstantiation
+	return cacheInstance, errorInInstantiation
 }
 
 // Put adds or replaces an entry in cache
-func (c Cache) Put(key string, val interface{}) {
+func (c *Cache) Put(key string, val interface{}) {
 	c.storage.Set(key, val, 0) // use deafult expiration time
 }
 
 // Get a particular key
-func (c Cache) Get(key string) (interface{}, bool) {
+func (c *Cache) Get(key string) (interface{}, bool) {
 	return c.storage.Get(key)
 }
 
 // Delete a key
-func (c Cache) Delete(key string) {
+func (c *Cache) Delete(key string) {
 	c.storage.Delete(key)
 }
 
 // Flush the items in the cache
-func (c Cache) Flush() {
+func (c *Cache) Flush() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.storage.Flush()
 	os.Remove(c.journalFile)
 }
 
 // Persist all the items in the cache to the disk
-func (c Cache) Persist() error {
+func (c *Cache) Persist() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	var cacheItems = c.storage.Items()
 	serializedCacheItems, err := json.Marshal(cacheItems)
 	if err != nil {
