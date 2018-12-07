608be4d90cf73f117003b17ed590273bf44c3ef8
Ankush Sharma <ankcodes@gmail.com>
2018-12-07 23:05:48 +0530

    Fixed disk persistance logic of cache

--- internal/pkg/cache/main.go
+++ internal/pkg/cache/main.go
@@ -1,9 +1,8 @@
 package cache
 
 import (
-	"encoding/json"
+	"encoding/gob"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"sync"
 	"time"
@@ -14,9 +13,10 @@ import (
 
 // Cache provides disk backed in-memory cache
 type Cache struct {
-	storage     *cache.Cache
-	journalFile string
-	mutex       sync.RWMutex
+	storage       *cache.Cache
+	journalFile   string
+	mutex         sync.RWMutex
+	cachableTypes []interface{}
 }
 
 var cacheInstance *Cache
@@ -27,39 +27,57 @@ var (
 	defaultCleanUpInterval    = 10 * time.Minute
 )
 
-// New returns a Cache instance
-func New(filename string) (*Cache, error) {
+// NewCache returns a Cache instance
+func NewCache(filename string, cachableTypes []interface{}) (*Cache, error) {
 	var errorInInstantiation error
 	once.Do(func() {
-		// load the disk persisted cache
-		bytesRead, err := ioutil.ReadFile(filename)
-		if err != nil {
-			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to load cache from the file %s", filename))
+		// register the types for decoding by gob
+		for _, cachable := range cachableTypes {
+			gob.Register(cachable)
 		}
-		var persistedCacheItems map[string]cache.Item
-		json.Unmarshal(bytesRead, persistedCacheItems)
+		// load the disk persisted cache
+		persistedCacheItems := make(map[string]cache.Item)
+		diskCacheFile, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
+		if err != nil {
+			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to open journal file %s for reading persisted cache items", filename))
+		}
+		dec := gob.NewDecoder(diskCacheFile)
+		err = dec.Decode(&persistedCacheItems)
+		if err != nil && err.Error() != "EOF" {
+			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to decode the cache from the file %s", filename))
+		}
 		cacheInstance = &Cache{
-			storage:     cache.NewFrom(defaultItemExpirationTime, defaultCleanUpInterval, persistedCacheItems),
-			journalFile: filename,
+			storage:       cache.NewFrom(defaultItemExpirationTime, defaultCleanUpInterval, persistedCacheItems),
+			journalFile:   filename,
+			cachableTypes: cachableTypes,
 		}
 	})
 	return cacheInstance, errorInInstantiation
 }
 
-// NewWithDefaults returns a configured Cache instance
-func NewWithDefaults(filename string, itemExpirationTime time.Duration, cleanUpInterval time.Duration) (*Cache, error) {
+// NewCacheWithDefaults returns a configured Cache instance
+func NewCacheWithDefaults(filename string, cachableTypes []interface{}, itemExpirationTime time.Duration, cleanUpInterval time.Duration) (*Cache, error) {
 	var errorInInstantiation error
 	once.Do(func() {
+		// register the types for decoding by gob
+		for _, cachable := range cachableTypes {
+			gob.Register(cachable)
+		}
 		// load the disk persisted cache
-		bytesRead, err := ioutil.ReadFile(filename)
+		diskCacheFile, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
 		if err != nil {
 			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to load cache from the file %s", filename))
 		}
 		var persistedCacheItems map[string]cache.Item
-		json.Unmarshal(bytesRead, persistedCacheItems)
+		dec := gob.NewDecoder(diskCacheFile)
+		err = dec.Decode(&persistedCacheItems)
+		if err != nil {
+			errorInInstantiation = errors.Wrap(err, fmt.Sprintf("Not able to decode the cache from the file %s", filename))
+		}
 		cacheInstance = &Cache{
-			storage:     cache.NewFrom(itemExpirationTime, cleanUpInterval, persistedCacheItems),
-			journalFile: filename,
+			storage:       cache.NewFrom(defaultItemExpirationTime, defaultCleanUpInterval, persistedCacheItems),
+			journalFile:   filename,
+			cachableTypes: cachableTypes,
 		}
 	})
 	return cacheInstance, errorInInstantiation
@@ -92,14 +110,20 @@ func (c *Cache) Flush() {
 func (c *Cache) Persist() error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
-	var cacheItems = c.storage.Items()
-	serializedCacheItems, err := json.Marshal(cacheItems)
+	// encode the cache items
+	diskCacheFile, err := os.OpenFile(c.journalFile, os.O_RDWR, 0600)
 	if err != nil {
-		return errors.Wrap(err, "Not able to unmarshall cache contents to json for disk persistance")
+		return errors.Wrap(err, fmt.Sprintf("Not able to open journal file %s for persisting cache", c.journalFile))
 	}
-	err = ioutil.WriteFile(c.journalFile, serializedCacheItems, os.FileMode(uint32(0777)))
+	cacheItems := c.storage.Items()
+	enc := gob.NewEncoder(diskCacheFile)
+	err = enc.Encode(cacheItems)
 	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Not able to persist cache items to disk file %s", c.journalFile))
+		return errors.Wrap(err, fmt.Sprintf("Not able to persist cache items to journal file %s", c.journalFile))
 	}
 	return nil
+}
+
+func (c *Cache) RegisterCachableType(cachable interface{}) {
+	c.cachableTypes = append(c.cachableTypes, cachable)
 }
